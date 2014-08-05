8af6ec4afbc0d965cc59e13f42b0ed660e3c4602
Zach Banks <zbanks@mit.edu>
2014-08-05 12:09:57 -0400

    Recovering after rm -r'ing directory

---
+++ grassroots.py
@@ -0,0 +1,249 @@
+import collections
+import json
+import os
+import uuid
+
+from werkzeug.serving import run_simple
+from werkzeug.routing import Map, Rule
+from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import SharedDataMiddleware
+
+__all__ = ("Field", "PropertyField", "CallableField", "JSONField", "Blade", "Root", "run")
+
+#
+# Fields. Expose values & functions over HTTP
+# 
+
+class Field(object):
+    """Generic property field. Constructor argument is default value"""
+    doc = "property"
+    def __init__(self, value=None):
+        self.value = value
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        return self.value
+
+    def __set__(self, obj, value):
+        self.value = value
+
+    def parse(self, obj, data):
+        """Redefine `parse` to change the way the value is deserialized (from JSON)"""
+        self.value = data
+
+    def export(self, obj):
+        """Redefine `export` to change the way the value is serialized (before being serialized to JSON)"""
+        return self.value
+
+class PropertyField(Field):
+    """Emulate property() behavior as a field"""
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+    def getter(self, fget):
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+    def setter(self, fset):
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+    def deleter(self, fdel):
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+    def parse(self, obj, data):
+        # Be a bit more leinient with what we accept here
+        if self.fset is not None:
+            self.fset(obj, data)
+
+    def export(self, obj):
+        # Be a bit more leinient with what we accept here
+        if self.fget is not None:
+            return self.fget(obj)
+
+class CallableField(Field):
+    """Turn a function into a (write-only) field
+    
+    If the field is 'set' to `v`, then the function is called using `v` for arguments.
+    If `v` is None, the function is called without arguments.
+    If `v` is a list (JSON Array), the function is called with positional arguments.
+    If `v` is a dict (JSON Object), the function is called with keyword arguments.
+    """
+    doc = "callable"
+    def __init__(self, fn):
+        self.value = fn
+        self.retval = None
+
+    def parse(self, obj, data=None):
+        # Call function on data
+        # Save return value for next time it is exported
+        if data is None:
+            self.retval = self.value(obj)
+        elif isinstance(data, list):
+            self.retval = self.value(obj, *data)
+        elif isinstance(data, dict):
+            self.retval = self.value(obj, **data)
+        else:
+            raise ValueError("Unable to call function. Argument must be a list, a dict, or None")
+
+    def export(self, obj):
+        # Export the last return value from the called function
+        # This is a little awkward, but it works
+        # XXX: Should the retval be destroyed on read?
+        return self.retval
+
+class JSONField(Field):
+    """Property field that serializes value with JSON."""
+    pass
+
+class BladeMeta(type):
+    """Metaclass which keeps track of Fields to expose over HTTP"""
+    fields = {}
+    references = {}
+    abstracts = {"Blade"}
+
+    def __new__(meta, name, bases, dct):
+        if name not in meta.abstracts:
+            if name not in meta.references:
+                meta.fields[name] = {}
+                meta.references[name] = {}
+
+            for key, val in dct.items():
+                if isinstance(val, Field):
+                    meta.fields[name][key] = val
+
+        return super(BladeMeta, meta).__new__(meta, name, bases, dct)
+
+    def __init__(cls, name, bases, dct):
+        super(BladeMeta, cls).__init__(name, bases, dct)
+
+    def __call__(cls, *args, **kwargs):
+        obj = super(BladeMeta, cls).__call__(*args, **kwargs)
+        #TODO: is there a better way of getting this?
+        classname = obj.__class__.__name__
+        if classname not in cls.abstracts:
+            cls.references[classname][id(obj)] = obj
+        return obj
+
+class Blade(object):
+    """Subclass this to export Fields"""
+    __metaclass__ = BladeMeta
+
+class Root(object):
+    """Webserver """
+    def __init__(self):
+        self.references = collections.defaultdict(dict)
+        self.ordering = {}
+        self.uuid = uuid.uuid4() #TODO: is this the right kind?
+
+        self.url_map = Map([
+            Rule("/root/<classname>/<cid>", endpoint="object"),
+            Rule("/root/<classname>", endpoint="class"),
+            Rule("/root", endpoint="all")
+        ])
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        adapter = self.url_map.bind_to_environ(environ)
+        try:
+            endpoint, values = adapter.match()
+            try:
+                json_resp = getattr(self, "app_" + endpoint)(request, **values)
+            except Exception as e:
+                # TODO: clean up error handling
+                json_resp = {"success": False, "error": str(e)}
+                raise
+        except:
+            json_resp = {"success": False, "error": "404"}
+            raise
+
+        headers = {"X-Server-UUID": self.uuid}
+
+        response = Response(json.dumps(json_resp), mimetype="application/json", headers=headers)
+
+        return response(environ, start_response)
+
+    def __call__(self, *args, **kwargs):
+        return self.wsgi_app(*args, **kwargs)
+
+    def app_object(self, request, classname, cid):
+        cid = int(cid)
+        if request.method == "GET":
+            return self.dump(classname, cid)
+        elif request.method in {"POST", "PUT"}:
+            try:
+                data = json.loads(request.data)
+            except ValueError:
+                print "Error parsing data"
+                raise
+            return self.load(classname, cid, data)
+
+    def app_class(self, request, classname):
+        return self.list_objects(classname)
+
+    def app_all(self, request):
+        # Discovery 
+        output = {}
+        for classname, fields in BladeMeta.fields.items():
+            output[classname] = {f: d.doc for f, d in fields.items()}
+        return output
+
+    def list_objects(self, classname=None):
+        if classname is None:
+            #return {clk : clv.keys() for clk, clv in self.references.items()}
+            #return [{"type": clk, "objects": clv.keys()} for clk, clv in self.references.items()]
+            return [clk for clk, clv in BladeMeta.references.items()]
+        else:
+            #return {cid : self.dump(classname, cid) for cid in self.references[classname]}
+            return [self.dump(classname, cid) for cid in BladeMeta.references[classname]]
+
+    def dump(self, classname, cid):
+        # Return a JSONable representation of an object
+        output = {}
+        #for key, value in self.references[classname][cid].__dict__.items():
+        obj = BladeMeta.references[classname][cid]
+        for fieldname, field in BladeMeta.fields[classname].items():
+            output[fieldname] = field.export(obj)
+        output["_id"] = cid
+        return output  
+    
+    def load(self, classname, cid, data):
+        # Set the JSON that changed
+        # cid = data["_id"] #???
+        obj = BladeMeta.references[classname][cid]
+        print 'data', data
+        print 'bm', BladeMeta.fields[classname]
+        for key, value in data.items():
+            if key in BladeMeta.fields[classname]:
+                field = BladeMeta.fields[classname][key]
+                field.parse(obj, value)
+        return self.dump(classname, cid)
+
+def run(app, host="127.0.0.1", port=8080):
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+        '/static': os.path.join(os.path.dirname(__file__), 'static')
+    })
+    run_simple(host, port, app, use_debugger=True, use_reloader=True)
+
+
---
+++ static/js/grass.js
@@ -0,0 +1,104 @@
+var makeBladeModel = function(type, typeData){
+    var properties = _.chain(typeData).pairs().map(function(x){ return x[1] == "property" ? x[0] : null }).compact().value();
+    properties.push("_id");
+    var callables = _.chain(typeData).pairs().map(function(x){ return x[1] == "callable" ? x[0] : null }).compact().value();
+
+    return Backbone.Model.extend({
+        idAttribute: "_id",
+        properties: properties,
+        callables: callables,
+        type: type,
+        url: function(){
+            return "/root/" + type + "/" + this.id;
+        },
+        parse: function(response, options){
+            // Only save properties, not callables
+            return _.pick(response, properties);
+        },
+        toJSON: function(options){
+            // Only send properties to server, not callables
+            return _.pick(this.attributes, properties);
+        },
+        call: function(key, args, options){
+            // A combination of `.set` and `.fetch`
+            var self = this;
+            if(!_.contains(callables, key)){
+                throw "Unable to call; function '" + key + "' does not exist.";
+            }
+            if(args === void 0){
+                args = null;
+            }
+
+            var options = options ? _.clone(options) : {};
+            var success = options.success;
+            var data = {};
+            data[key] = args;
+            var params = {
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                success: function(response){
+                    var retval = response[key];
+                    if (!self.set(self.parse(response, options), options)) return false;
+                    if (success) success(self, retval, options);
+                    self.trigger('sync', self, response, options);
+                    self.trigger('called:' + key, self, retval, options);
+                }
+            };
+            this.sync('create', this, _.defaults(params, options));
+        },
+        initialize: function(){
+
+        }
+    });
+}
+
+var makeBladeCollection = function(type, typeData, model){
+    return Backbone.Collection.extend({
+        model: model,
+        url: "/root/" + type,
+        type: type,
+        initialize: function(){
+        }
+    });
+}
+
+var Root = Backbone.Model.extend({
+    url: "/root",
+    Collections: {},
+    Models: {},
+    initialize: function(){
+        var self = this;
+        this.listenToOnce(this, "change", function(model, options){
+            console.log("first change", model.attributes);
+            _.each(model.keys(), function(name){
+                var mdl = self.Models[name] = makeBladeModel(name, model.get(name));
+                self.Collections[name] = makeBladeCollection(name, model.get(name), mdl);
+            });
+
+            // Next time the model changes, that's bad!
+            this.listenTo(this, "change", function(model, options){
+                // This isn't good!
+                console.log("Uh oh, model changed types!", model);
+                Backbone.trigger("typeChange");
+                // Maybe fire an event for the particular attrs that changed?
+            });
+
+            this.trigger("typesLoaded");
+        });
+    }
+});
+
+var root = new Root();
+root.fetch();
+Backbone.listenToOnce(root, "typesLoaded", function(){
+    Timings = new root.Collections.Timing;
+
+    Backbone.listenTo(Timings, "add", function(model){
+        t = model;
+    });
+
+    Timings.fetch();
+});
+
+//run = function(){t.fetch({success: function(a){ a.fetch({success: function(b){ if(c){run();} }}); }});}
+
